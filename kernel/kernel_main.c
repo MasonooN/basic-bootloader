830f7d2b3f087dff4bd4c609af3d22419134bc37
Ash <87194661+knvtva@users.noreply.github.com>
2024-08-22 22:57:02 +0100

    Update kernel_main.c

--- kernel/kernel_main.c
+++ kernel/kernel_main.c
@@ -1,13 +1,12 @@
 void kernel_main() {
-    const char* message = "Hello CPU, from the kernel."; // I just put a basic message here because... why not?
-    char* video_memory = (char*)0xb8000;
+    const char *message = "Hello CPU, from the kernel."; // I just put a basic message here because... why not?
+    char *video_memory = (char *)0xb8000;
     unsigned short offset = 0;
 
-    // Loop through each character to display it on the screen.
     while(*message) {
-        video_memory[offset++] = *message; // Write the character to the screen
+        video_memory[offset++] = *message++; // Write the character to the screen
         video_memory[offset++] = 0x07;
     }
 
-    while(1) {}
+    while (1) {}
 }
